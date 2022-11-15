c4c7c64c76b4241439c8cd4cdee4b2471a2f8065
vahide <100282383+ashenhair@users.noreply.github.com>
2022-11-15 09:48:12 +0300

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react"
+import SearchBox from "./SearchBox";
+import {SlMenu} from "react-icons/sl";
+
+
+
+
+
+function Navbar() {
+
+
+
+ 
+
+    return (
+      <>
+      {" "}
+      <SlMenu/>  {" "}
+
+      <nav className="navBar">
+      
+   
+      </nav>
+
+
+      <SearchBox/>
+   
+      </>
+    );
+  }
+   
+  export default Navbar;
+
+ 
