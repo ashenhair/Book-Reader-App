c485d597431c2e52414a5ced19a1a83975438014
vahide <100282383+ashenhair@users.noreply.github.com>
2022-11-21 16:12:20 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,65 @@
 import React, { useState } from "react"
 import SearchBox from "./SearchBox";
-import {SlMenu} from "react-icons/sl";
-
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { SecretBarData } from "./SecretBarData";
 
 
 
 
 function Navbar() {
 
+  const [secretbar, setSecretbar] = useState(false);
 
+  const showSecretbar = () => setSecretbar(!secretbar);
 
- 
 
     return (
       <>
-      {" "}
-      <SlMenu/>  {" "}
+      <div className="navbar">
+        <Link to='#' className='menu-bars'>
+          <FaIcons.FaBars  onClick={showSecretbar}/>
+        </Link>
+      </div>
+      <nav className={secretbar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className="nav-menu-items" onClick={showSecretbar}>
+          <li className="navbar-toggle">
+            <Link to='#' className='menu-bars'>
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+          {SecretBarData.map((item,index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+        </nav>  
 
-      <nav className="navBar">
+    
+     <nav className="navbar" >
       
-   
-      </nav>
-
-
+     </nav>
+     
       <SearchBox/>
-   
-      </>
-    );
-  }
+    </>
+  );
+}
+
+    
+
+      
+
+  
    
   export default Navbar;
+
+ 
 
  
