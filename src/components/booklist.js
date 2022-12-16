7d1d818fd155c0cdd0d3127998e59e18282085ae
vahide <100282383+ashenhair@users.noreply.github.com>
2022-12-16 13:30:18 +0300

    Add files via upload

---
+++ src/components/booklist.js
@@ -0,0 +1,40 @@
+export const booklist = 
+[
+    {
+      id: 1,
+      imageLink: "images/Eat.jpg",
+      title: "Eat, Pray, Love"
+      
+    },
+
+    {
+         id: 2,
+         imageLink : "images/john.jpg",
+         title : "Of Mice And Me"
+        
+      },
+
+      {
+        id: 3,
+        imageLink: "images/mini.jpg",
+        title: "Minimalist Lifestyle"
+        
+      },
+
+      {
+        id: 4,
+        imageLink: "images/sherlock.jpg",
+        title: "Sherlock Holmes"
+        
+      },
+
+      {
+        id : 5,
+        imageLink: "images/thecodebroker.jpg",
+        title: "The Code Broker"
+        
+      }
+    ] 
+
+    
+
